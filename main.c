6e528270a59694b4314058705f40d9230521c984
Nicholas Dapprich <nicholasdapprich@gmail.com>
2019-04-30 17:32:04 -0500

    updated main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * Project2.c
  *
  * Created: 4/30/2019 5:08:04 PM
- * Author : nicho
+ * Author : nicholas
  */ 
 
 #include <avr/io.h>
@@ -10,7 +10,13 @@
 
 int main(void)
 {
-    /* Replace with your application code */
+    /* Replace with your application code 
+	Pattern Chase game with 3 difficulties
+	level 1 Easy: length = 4, delay = 1000ms
+	level 2 Medium: length = 7, delay = 750ms
+	level 3 Hard: length 10, delay = 500ms
+	One button for changing difficulty, output status to terminal
+	*/
     while (1) 
     {
     }
