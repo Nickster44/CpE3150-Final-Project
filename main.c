be8b983e432f6a2e48a49b27503ac20a7b98b590
Nicholas Dapprich <nicholasdapprich@gmail.com>
2019-04-30 17:09:44 -0500

    added main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Project2.c
+ *
+ * Created: 4/30/2019 5:08:04 PM
+ * Author : nicho
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
